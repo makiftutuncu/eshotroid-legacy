8937f5a58f08b23de7be62d24f5c6cf043f23e8c
mehmetakiftutuncu <m.akif.tutuncu@gmail.com>
2013-03-10 02:11:26 +0200

    Empty project with a custom icon

---
+++ Eshotroid/src/com/mehmetakiftutuncu/eshotroid/Main.java
@@ -0,0 +1,14 @@
+package com.mehmetakiftutuncu.eshotroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}

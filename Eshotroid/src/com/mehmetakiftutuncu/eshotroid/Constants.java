cc91af1aadb5085d301fb1f3d1a3b17a578f236d
mehmetakiftutuncu <m.akif.tutuncu@gmail.com>
2013-04-13 23:26:13 +0300

    * Bus lines are stored on the external storage now and read from there.

--- Eshotroid/src/com/mehmetakiftutuncu/eshotroid/Constants.java
+++ Eshotroid/src/com/mehmetakiftutuncu/eshotroid/Constants.java
@@ -7,8 +7,16 @@ package com.mehmetakiftutuncu.eshotroid;
  */
 public class Constants
 {
+	/**	Package name of the application */
+	public static String PACKAGE_NAME;
+	
+	/**	URL of the mobile ESHOT web site to which requests will be sent to get bus lines */
 	public static final String BUS_LINES_URL = "http://mobil.eshot.gov.tr/Ulasim.aspx";
+	/**	URL of the mobile ESHOT web site to which requests will be sent to get times of a specific bus line */
 	public static final String BUS_TIMES_URL = "http://mobil.eshot.gov.tr/Saatler.aspx";
+	
+	/**	Type parameter of the {@link BUS_TIMES_URL} */
 	public static final String TYPE_PARAMETER = "tip";
+	/**	Line parameter of the {@link BUS_TIMES_URL} */
 	public static final String LINE_PARAMETER = "hatno";
 }
--- Eshotroid/src/com/mehmetakiftutuncu/eshotroid/Main.java
+++ Eshotroid/src/com/mehmetakiftutuncu/eshotroid/Main.java
@@ -1,17 +1,16 @@
 package com.mehmetakiftutuncu.eshotroid;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.mehmetakiftutuncu.eshotroid.model.BusLine;
-import com.mehmetakiftutuncu.eshotroid.model.BusTime;
 import com.mehmetakiftutuncu.eshotroid.utilities.GetPageTask;
 import com.mehmetakiftutuncu.eshotroid.utilities.Parser;
 import com.mehmetakiftutuncu.eshotroid.utilities.Processor;
+import com.mehmetakiftutuncu.eshotroid.utilities.StorageManager;
 
 /**
  * Main activity of the application
@@ -31,83 +30,52 @@ public class Main extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
+		Constants.PACKAGE_NAME = getPackageName();
+		
 		initialize();
 		
-		Log.i(LOG_TAG, "Getting all bus lines...");
-		GetPageTask task = new GetPageTask(this);
-		task.execute(Constants.BUS_LINES_URL);
-		
-		try
-		{
-			String busLinesPage = task.get();
-			
-			if(busLinesPage != null)
-			{
-				ArrayList<String> busLines = Parser.parseBusLines(busLinesPage);
-				
-				Log.i(LOG_TAG, "These are what we get as bus lines from the page:");
-				for(String i : busLines)
-				{
-					Log.i(LOG_TAG, i);
-				}
-				
-				Log.i(LOG_TAG, "Let\'s process these motherfuckers.");
-				ArrayList<BusLine> busLineObjects = Processor.processBusLines(busLines);
-				
-				Log.i(LOG_TAG, "Cool! Now let\'s get the times for a random bus.");
-				BusLine randomBus = busLineObjects.get(new Random().nextInt(busLineObjects.size()));
-				String url = String.format("%s?%s=%s&%s=%s", 	Constants.BUS_TIMES_URL,
-																Constants.TYPE_PARAMETER,
-																"H",
-																Constants.LINE_PARAMETER,
-																randomBus.getNumber());
-				
-				Log.i(LOG_TAG, "Getting bus times for " + randomBus.getNumber() + "...");
-				task = new GetPageTask(this);
-				task.execute(url);
-				
-				try
-				{
-					String busTimesPage = task.get();
-					
-					if(busTimesPage != null)
-					{
-						ArrayList<String> busTimes = Parser.parseBusTimes(busTimesPage);
-						
-						Log.i(LOG_TAG, "These are what we get as bus times from the page:");
-						for(String i : busTimes)
-						{
-							Log.i(LOG_TAG, i);
-						}
-						
-						Log.i(LOG_TAG, "Let\'s process these motherfuckers as well.");
-						ArrayList<BusTime> busTimeObjects = Processor.processBusTimes(busTimes);
-						
-						Log.i(LOG_TAG, "Cool! Cool, cool, cool! Now let\'s see the final result.");
-						
-						Log.i(LOG_TAG, "Bus: " + randomBus.getNumber());
-						Log.i(LOG_TAG, randomBus.getSource() + " / " + randomBus.getDestination());
-						for(BusTime i : busTimeObjects)
-						{
-							Log.i(LOG_TAG, i.getTimeFromSource() + " / " + i.getTimeFromDestination());
-						}
-						
-						Log.i(LOG_TAG, "Voila!");
-					}
-				}
-				catch(Exception e)
-				{
-					Log.e(LOG_TAG, "Couldn't load bus times!", e);
-				}
-			}
-		}
-		catch(Exception e)
-		{
-			Log.e(LOG_TAG, "Couldn't load bus lines!", e);
-		}
+		loadBusLines();
 	}
 	
+	/**	Initializes components */
 	private void initialize()
 	{
+	}
+	
+	/**	Loads bus lines from external storage if possible, if not tries to download them. */
+	private void loadBusLines()
+	{
+		ArrayList<BusLine> busLines = StorageManager.readBusLines();
+		if(busLines != null)
+		{
+			Log.d(LOG_TAG, "Bus lines already exist on the external storage.");
+		}
+		else
+		{
+			Log.d(LOG_TAG, "Bus lines don't exist on the external storage. Trying to download...");
+			
+			GetPageTask task = new GetPageTask(this);
+			task.execute(Constants.BUS_LINES_URL);
+			
+			try
+			{
+				String busLinesPage = task.get();
+				
+				if(busLinesPage != null)
+				{
+					ArrayList<String> parsedBusLines = Parser.parseBusLines(busLinesPage);
+					busLines = Processor.processBusLines(parsedBusLines);
+					
+					// Now that we downloaded bus lines, let's store them
+					StorageManager.storeBusLines(busLines);
+				}
+			}
+			catch(Exception e)
+			{
+				Log.e(LOG_TAG, "Couldn't download bus times!", e);
+			}
+		}
+		
+		// TODO: Set the list or whatever
 	}
 }
